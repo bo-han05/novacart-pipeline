55e4404c0f8b9fdc7867b91a75491391ef519751
Bo Han <bohan@dhcp-9-65-177-90.hk8-us.ibm.com>
2026-07-01 16:32:31 -0500

    inspecting data

--- notebooks/inspect_data.py
+++ notebooks/inspect_data.py
@@ -1,9 +1,23 @@
 import sqlite3
+import json
 import pandas as pd
 
-conn = sqlite3.connect('/Users/bohan/Downloads/novacart_project/delta-lakehouse-pipeline/data/landing/products.db')
-df = pd.read_sql("SELECT * FROM products", conn)
-df.to_csv('/Users/bohan/Downloads/novacart_project/delta-lakehouse-pipeline/data/landing/products/products.csv', index=False)
+# conn = sqlite3.connect('/Users/bohan/Downloads/novacart_project/delta-lakehouse-pipeline/data/landing/products.db')
+# df = pd.read_sql("SELECT * FROM products", conn)
+# df.to_csv('/Users/bohan/Downloads/novacart_project/delta-lakehouse-pipeline/data/landing/products/products.csv', index=False)
 
-print(f"Exported {len(df)} rows to products.csv")
-conn.close()
+# print(f"Exported {len(df)} rows to products.csv")
+# conn.close()
+
+with open('/Users/bohan/Downloads/novacart_project/delta-lakehouse-pipeline/data/landing/customers/customers.json') as f:
+    data = json.load(f)
+
+print("=== CUSTOMERS ===")
+print(type(data))
+print(json.dumps(data[0], indent=2))
+
+# orders
+# print("\n=== ORDERS ===")
+# df = pd.read_csv('/Users/bohan/Downloads/novacart_project/delta-lakehouse-pipeline/data/landing/orders/orders.csv')
+# print(df.columns.tolist())
+# print(df.head())
